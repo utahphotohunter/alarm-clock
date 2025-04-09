5bb14a8ff9d08f3346b0249cbf63d0cf29bfa9ad
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-04-08 23:34:21 -0600

    allowed api key to be public for testing

---
+++ scripts/keys/keys.mjs
@@ -0,0 +1,10 @@
+// api key for news articles 
+
+// ==================================================
+// api key returns
+// ==================================================
+
+// rapid api key
+export function rapidApiKey() {
+    return 'e9f8fd2c4amsh34446a9d279ae47p19da6ajsn14af066120f5';
+}
